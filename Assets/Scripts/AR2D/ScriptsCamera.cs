7f647b090b30592989ea406910b3d501e560c2a5
nikkitwo <nikki860618@gmail.com>
2019-09-25 04:51:25 +0800

    添加移動旋轉縮放模型功能 , 添加前鏡頭使用

--- Assets/Scripts/AR2D/ScriptsCamera.cs
+++ Assets/Scripts/AR2D/ScriptsCamera.cs
@@ -5,58 +5,115 @@ using UnityEngine.UI;
 public class ScriptsCamera : MonoBehaviour
 {
     private bool camAvailable;
-    private WebCamTexture backCam;
-    private WebCamTexture frontCam;
+    private WebCamTexture runningWebcam;
     private Texture defaultBackground;
 
     public RawImage background;
-    public AspectRatioFitter fit;
+    private int lastRotationAngle = 0;
+    public AspectRatioFitter fitter;
 
-    // Use this for initialization
-    private void Start()
-    {
+    IEnumerator DoStartWebcam(){
+        StopWebcam();
+        yield return new WaitForEndOfFrame();
+
         defaultBackground = background.texture;
         WebCamDevice[] devices = WebCamTexture.devices;
         if (devices.Length == 0)
         {
             Debug.Log("No camera detected");
             camAvailable = false;
-            return;
+            yield break;
         }
+
         for (int i = 0; i < devices.Length; i++)
         {
-
-            //            if (!devices [i].isFrontFacing) {    //開啟後鏡頭
-            if (devices[i].isFrontFacing)
-            {    //開啟前鏡頭
-                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            if (!devices [i].isFrontFacing) {
+                runningWebcam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
             }
         }
-        if (backCam == null)
-        {
+
+        if (runningWebcam == null) {
             Debug.Log("Unable to find back camera");
-            return;
+            yield break;
         }
-        backCam.Play();
-        background.texture = backCam;
+
+        runningWebcam.Play();
+        background.texture = runningWebcam;
 
         camAvailable = true;
     }
 
-    // Update is called once per frame
-    private void Update()
+    public void StartWebcam(){
+        StartCoroutine(DoStartWebcam());
+    }
+
+    public void StopWebcam(){
+        if(runningWebcam == null)
+            return;
+
+        runningWebcam.Stop();
+        runningWebcam = null;
+        camAvailable = false;
+    }
+
+    void JudgeCamera(){
+        //int rotAngle = -runningWebcam.videoRotationAngle;
+        //while( rotAngle < 0 ) rotAngle += 360;
+        //while( rotAngle > 360 ) rotAngle -= 360;
+
+        if (runningWebcam != null && lastRotationAngle != runningWebcam.videoRotationAngle)
+        {
+            OnOrientationChanged();
+            lastRotationAngle = runningWebcam.videoRotationAngle;
+        }
+    }
+
+    private void OnOrientationChanged()
+    {
+        // 旋转rawimage，为什么加一个负号呢？因为rawimage的z轴是背对图像的，直接使用videoRotationAngle旋转，相对于图片是逆时针旋转
+        background.transform.localRotation = Quaternion.Euler(0, 0, -runningWebcam.videoRotationAngle);
+
+        // 判断是否是竖屏，竖屏时由于旋转的关系，需要将width和height调换
+        if (runningWebcam.videoRotationAngle % 180 != 0)
+            background.rectTransform.sizeDelta = new Vector2(Screen.height, Screen.width);
+        else
+            background.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+        /*
+        if (runningWebcam.videoRotationAngle % 180 != 0)
+            background.rectTransform.sizeDelta = new Vector2(Display.main.systemHeight, Display.main.systemWidth);
+        else
+            background.rectTransform.sizeDelta = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
+        */
+    }
+
+    void Start()
+    {
+        StartWebcam();
+    }
+
+    void Update()
     {
         if (!camAvailable)
             return;
 
-        float ratio = (float)backCam.width / (float)backCam.height;
-        fit.aspectRatio = ratio;
+        JudgeCamera();
 
-        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
-        //        background.rectTransform.localScale = new Vector3 (1f, scaleY, 1f);    //非鏡像
-        background.rectTransform.localScale = new Vector3(-1f, scaleY, 1f);    //鏡像
+        if(!fitter)
+            return;
 
-        int orient = -backCam.videoRotationAngle;
+        float ratio = (float)runningWebcam.width / (float)runningWebcam.height;
+        fitter.aspectRatio = ratio;
+
+        float scaleY = runningWebcam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3 (1f, scaleY, 1f);    //非鏡像
+        //background.rectTransform.localScale = new Vector3(-1f, scaleY, 1f);    //鏡像
+
+        int orient = -runningWebcam.videoRotationAngle;
         background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+
+    void OnApplicationQuit() {
+        StopWebcam();
     }
 }
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/* Script that controls dragging the furniture augmentations on the screen. */
+public class MoveController : MonoBehaviour
+{
+    public Transform BasePoint;
+    public float RotateSpeed = 25;
+    private Transform _activeObject = null;
+    private Camera ControllerCamera;
+
+    private Vector3 _startObjectPosition;
+    private Vector2 _startTouchPosition;
+    private Vector2 _touchOffset;
+
+    private void Start() {
+        ControllerCamera = GetComponent<Camera>();
+    }
+
+    public Transform ActiveObject {
+        get {
+            return _activeObject;
+        }
+    }
+
+    public void SetMoveObject(Transform newMoveObject) {
+        if (newMoveObject.gameObject.tag == "ARObject") {
+            _activeObject = newMoveObject;
+            _startObjectPosition = _activeObject.position;
+            
+            if(Input.touchCount > 0)
+                _startTouchPosition = Input.GetTouch(0).position;
+            else
+                _startTouchPosition = Input.mousePosition;
+
+            _touchOffset = ControllerCamera.WorldToScreenPoint(_startObjectPosition);
+        }
+    }
+
+    void Update () {
+        
+        #if UNITY_EDITOR
+        if(Input.GetMouseButton(0)){
+            Vector2 mousePosition = Input.mousePosition;
+            RaycastHit hit;
+
+            if (_activeObject == null) {
+                if(Physics.Raycast(ControllerCamera.ScreenPointToRay(mousePosition), out hit)) {
+                    SetMoveObject(hit.transform);
+                }
+            }
+
+            if (_activeObject != null) {
+                Vector2 screenPosForRay = (mousePosition - _startTouchPosition) + _touchOffset;
+
+                Vector3 newPos = ControllerCamera.ScreenToWorldPoint(new Vector3(screenPosForRay.x, screenPosForRay.y, BasePoint.position.z));
+
+                _activeObject.position = Vector3.Lerp(_activeObject.position, newPos, 0.25f);
+
+                _activeObject.Rotate(0, RotateSpeed * Time.deltaTime, 0);
+            }
+
+        }
+        #endif
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            RaycastHit hit;
+
+            /* If we're currently not dragging any augmentation, do a raycast to find one in the scene. */
+            if (_activeObject == null) {
+                if (Physics.Raycast(ControllerCamera.ScreenPointToRay(touch.position), out hit)) {
+                    SetMoveObject(hit.transform);
+                }
+            }
+
+            if (_activeObject != null) {
+                Vector2 screenPosForRay = (touch.position - _startTouchPosition) + _touchOffset;
+
+                Vector3 newPos = ControllerCamera.ScreenToWorldPoint(new Vector3(screenPosForRay.x, screenPosForRay.y, BasePoint.position.z));
+
+                _activeObject.position = Vector3.Lerp(_activeObject.position, newPos, 0.25f);
+
+                if(Input.touchCount == 1)
+                    _activeObject.Rotate(0, RotateSpeed * Time.deltaTime, 0);
+            }
+
+            /* If we are dragging an augmentation, raycast against the ground plane to find how the augmentation should be moved. 
+            if (_activeObject != null) {
+                var screenPosForRay = (touch.position - _startTouchPosition) + _touchOffset;
+
+                
+                Ray cameraRay = Camera.main.ScreenPointToRay(screenPosForRay);
+                Plane p = new Plane(Vector3.up, Vector3.zero);
+
+                float enter;
+                if (p.Raycast(cameraRay, out enter)) {
+                    var position = cameraRay.GetPoint(enter);
+
+                    /* Clamp the new position within reasonable bounds and make sure the augmentation is firmly placed on the ground plane. 
+                    position.x = Mathf.Clamp(position.x, -15.0f, 15.0f);
+                    position.y = 0.0f;
+                    position.z = Mathf.Clamp(position.z, -15.0f, 15.0f);
+
+                    /* Lerp the position of the dragged augmentation so that the movement appears smoother 
+                    _activeObject.position = Vector3.Lerp(_activeObject.position, position, 0.25f);
+                }
+            }*/
+        }
+
+        if(Input.touchCount == 0 && !Input.GetMouseButton(0)){
+            /* If there are no touches, stop dragging the currently dragged augmentation, if there is one. */
+            _activeObject = null;
+        }
+    }
+}
