85922176e335e25bba257e291876c8ab7ee4b78f
nikkitwo <nikki860618@gmail.com>
2019-09-27 02:58:28 +0800

    新增 Android Version 自動++

---
+++ Assets/Scripts/Editor/AutoIncreaseBundle.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+class AutoIncreaseBundle : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log("Auto Increase bundleVersionCode...");
+        PlayerSettings.Android.bundleVersionCode++;
+    }
+}
+#endif
