d7af34369dd074d302ed6b0381ccc92bcb057c1f
nikkitwo <nikki860618@gmail.com>
2019-10-28 21:12:38 +0800

    v1.22
    新增角度限制

--- Assets/Scripts/AR3D/AR3DLayout.cs
+++ Assets/Scripts/AR3D/AR3DLayout.cs
@@ -8,17 +8,68 @@ public class AR3DLayout : MonoBehaviour
 {
     public static AR3DLayout instance;
 
+    public Camera ARCamera;
     public Button BTNExit;
     public Button BTNShot;
     public Button BTNTracking;
+    public Text TXTFacingAngle;
+    public Button BTNDistance;
+    public Text TXTDistance;
     public Transform ArtworkPool;
 
     public GameObject HelpCanvas;
 
+    int displayType;
+    GameObject currentStreetPhoto;
+
     SaveScreen saveScreen;
+
+    float updateIndex = 0;
+    float updateDelay = 0.33f;
 
     void Awake(){
         instance = this;
+    }
+
+    void CheckAngleAndButton(float angle){
+        if(displayType == 0)
+        {
+            if(angle > 65 && angle < 75){
+                BTNTracking.interactable = true;
+                TXTFacingAngle.color = Color.green;
+            } else {
+                BTNTracking.interactable = false;
+                TXTFacingAngle.color = Color.red;
+            }
+        } 
+        else if(displayType == 1)
+        {
+            if(angle > 80){
+                BTNTracking.interactable = true;
+                TXTFacingAngle.color = Color.green;
+            } else {
+                BTNTracking.interactable = false;
+                TXTFacingAngle.color = Color.red;
+            }
+        }
+    }
+
+    void Update(){
+        if(updateIndex > updateDelay){
+            float angle = GetFacingAngle();
+            TXTFacingAngle.text = angle.ToString("0");
+            updateIndex = 0;
+            CheckAngleAndButton(angle);
+
+            TXTDistance.text = Vector3.Distance(ARCamera.transform.position, Vector3.zero).ToString("0.00");
+        }
+        updateIndex += Time.deltaTime;
+
+        if(displayType == 1 && currentStreetPhoto != null){
+            Vector3 frontVec = (-ARCamera.transform.position).normalized * 2;
+            Vector3 photoPos = ARCamera.transform.position + frontVec;
+            currentStreetPhoto.transform.position = photoPos;
+        }
     }
 
     void OnEnable() {
@@ -27,6 +78,7 @@ public class AR3DLayout : MonoBehaviour
         Screen.orientation = ScreenOrientation.AutoRotation;
 
         HelpCanvas.SetActive(true);
+        updateIndex = 0;
     }
 
     void OnDisable() {
@@ -40,6 +92,7 @@ public class AR3DLayout : MonoBehaviour
         BTNExit.onClick.AddListener(DoExit);
         BTNShot.onClick.AddListener(DoShot);
         BTNTracking.onClick.AddListener(DoStartTracking);
+        BTNDistance.onClick.AddListener(DoDistance);
 
         saveScreen = GetComponent<SaveScreen>();
     }
@@ -49,6 +102,8 @@ public class AR3DLayout : MonoBehaviour
         {
             Destroy(item.gameObject);
         }
+
+        displayType = 0;
 
         if(obj)
             Instantiate(obj, ArtworkPool);
@@ -60,8 +115,10 @@ public class AR3DLayout : MonoBehaviour
             Destroy(item.gameObject);
         }
 
+        displayType = 1;
+
         if(obj)
-            Instantiate(obj, ArtworkPool);
+            currentStreetPhoto = Instantiate(obj, ArtworkPool);
     }
 
     void DoExit(){
@@ -75,4 +132,15 @@ public class AR3DLayout : MonoBehaviour
     void DoStartTracking(){
         VoidAR.GetInstance().startMarkerlessTracking();
     }
+
+    void DoDistance(){
+        if(TXTDistance.gameObject.activeSelf == false) TXTDistance.gameObject.SetActive(true);
+        else TXTDistance.gameObject.SetActive(false);
+    }
+
+    public float GetFacingAngle(){
+        return Mathf.Asin(-Mathf.Clamp(Input.acceleration.y, -1, 1)) *  Mathf.Rad2Deg;
+    }
+
+    
 }
